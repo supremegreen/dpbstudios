5df8c7b8d3b9cb322777dc46440a1e348d4d08e2
iamQGibson <2qgibson@gmail.com>
2026-05-24 20:20:50 -0400

    Updated dashboard

---
+++ config.js
@@ -0,0 +1,9 @@
+// BUSINESS CONFIGURATION
+
+const BUSINESS_NAME = 'bestfriends'
+
+// STRIPE PAYMENT LINK
+// Success URL is configured INSIDE Stripe dashboard
+
+const PAYMENT_LINK =
+  'https://buy.stripe.com/test_00w3cvgfE06F5ho6gSgUM00'
