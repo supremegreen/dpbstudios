e649502530c043212e62a629dacaf534d775e03a
iamQGibson <2qgibson@gmail.com>
2026-05-30 23:41:50 -0400

    Updated dashboard

--- blacklarry/config.js
+++
@@ -1,9 +0,0 @@
-// BUSINESS CONFIGURATION
-
-const BUSINESS_NAME = 'blacklarry'
-
-// STRIPE PAYMENT LINK
-// Success URL is configured INSIDE Stripe dashboard
-
-const PAYMENT_LINK =
-  'https://buy.stripe.com/9B6fZh3sScTr7pwdJkgUM0b'
